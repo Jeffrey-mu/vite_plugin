53d92ae87b2a08430f8cfe0b239219736cac3212
mr-wang1009 <1115378579@qq.com>
2022-07-17 16:55:33 +0800

    update: code

--- my_plugin.js
+++ my_plugin.js
@@ -1,33 +1,50 @@
+import { URLSearchParams } from 'url'
 export default function (options) {
+  let index = 1
   return {
     name: 'mu_plugin',
     resolveId(id) {
       // console.log(id);
     },
     load(id) {
+
+      // return {
+      //   moduleId,
+      //   query,
+      //   pageId,
+      // }
     },
 
     transform(src, id) {
+      console.log('transform');
+      // if (id.indexOf('.vue')) {
+      //   return src.replace(/console.log\(.*\)/,
+      //     ``)
+      // }
     },
     options(options) {
       console.log('options');
+      console.log(options);
     },
     // vite hook
     config(config) {
       console.log('config');
     },
     buildStart(buildStart) {
+      // console.log(buildStart);
       console.log('buildStart');
     },
     configResolved(configResolved) {
-      console.log('configResolved');
       console.log(configResolved);
+      console.log('configResolved');
+    },
+    configureServer(server) {
+      console.log('server')
     },
     transformIndexHtml(html) {
-      console.log(html);
       return html.replace(
-        /<title>(.*?)<\/title>/,
-        `<title>Title replaced!</title>`
+        /console.log\(.*\)/,
+        ``
       )
     }
   }
